174251112d24a0e98352efe1a44420b5e2687097
Jackson Buck <mout12@gmail.com>
2025-11-14 18:16:34 -0800

    Multiple enemies - one is randomly selected

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿using fights;
+﻿using System;
+using System.Collections.Generic;
+using fights;
 
-var stick = new Weapon(name: "Stick", damage: 1);
-var dagger = new Weapon(name: "Dagger", damage: 5);
+var jackson = new Fighter(
+    name: "Jackson",
+    health: 100,
+    weapon: new Weapon(name: "Dagger", damage: 5));
 
-var fighterOne = new Fighter(name: "Jackson", health: 100, weapon: dagger);
-var fighterTwo = new Fighter(name: "Nerd", health: 120, weapon: stick);
+var enemies = new List<Fighter>
+{
+    new Fighter(name: "Nerd", health: 120, weapon: new Weapon(name: "Stick", damage: 1)),
+    new Fighter(name: "Goblin", health: 80, weapon: new Weapon(name: "Rusty Blade", damage: 4)),
+    new Fighter(name: "Orc Warrior", health: 140, weapon: new Weapon(name: "Heavy Club", damage: 7)),
+    new Fighter(name: "Skeleton Knight", health: 110, weapon: new Weapon(name: "Ancient Sword", damage: 6))
+};
 
-var fight = new Fight(fighterOne, fighterTwo);
+var random = new Random();
+var enemy = enemies[random.Next(enemies.Count)];
+
+var fight = new Fight(jackson, enemy);
 fight.Start();
