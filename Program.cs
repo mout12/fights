7ab61f7146aaa5b76580b6021dae7f54d8285d6d
Jackson Buck <mout12@gmail.com>
2025-11-22 10:22:24 -0800

    Get rid of the GetOrCreateWeapon/Armor stuff.  That was not easy to use.

--- Program.cs
+++ Program.cs
@@ -5,27 +5,95 @@ using fights;
 var weaponCatalog = new Dictionary<string, IWeapon>(StringComparer.OrdinalIgnoreCase);
 var armorCatalog = new Dictionary<string, IArmor>(StringComparer.OrdinalIgnoreCase);
 
-IWeapon GetOrCreateWeapon(string name, int damage, Func<IWeapon>? factory = null)
+IWeapon RegisterWeapon(IWeapon weapon)
+{
+    weaponCatalog[weapon.Name] = weapon;
+    return weapon;
+}
+
+IWeapon GetWeapon(string name)
 {
     if (!weaponCatalog.TryGetValue(name, out var weapon))
     {
-        weapon = factory?.Invoke() ?? new Weapon(name, damage);
-        weaponCatalog[name] = weapon;
+        throw new InvalidOperationException($"Unknown weapon '{name}'.");
     }
 
     return weapon;
 }
 
-IArmor GetOrCreateArmor(string name, int defense)
+IArmor RegisterArmor(IArmor armor)
+{
+    armorCatalog[armor.Name] = armor;
+    return armor;
+}
+
+IArmor GetArmor(string name)
 {
     if (!armorCatalog.TryGetValue(name, out var armor))
     {
-        armor = new Armor(name, defense);
-        armorCatalog[name] = armor;
+        throw new InvalidOperationException($"Unknown armor '{name}'.");
     }
 
     return armor;
 }
+
+void RegisterWeapons()
+{
+    var weapons = new IWeapon[]
+    {
+        new Weapon(name: "Fists", damage: 1),
+        new Weapon(name: "Stick", damage: 1),
+        new Weapon(name: "Dagger", damage: 5),
+        new Weapon(name: "Short Sword", damage: 7),
+        new Weapon(name: "War Axe", damage: 9),
+        new SelfDamagingWeapon(name: "Morningstar Flail", damage: 12, selfDamage: 1),
+        new Weapon(name: "Wizard Staff", damage: 11),
+        new Weapon(name: "Rusty Blade", damage: 4),
+        new Weapon(name: "Fangs", damage: 5),
+        new Weapon(name: "Shiv", damage: 6),
+        new Weapon(name: "Heavy Club", damage: 7),
+        new Weapon(name: "Ancient Sword", damage: 6),
+        new Weapon(name: "Shadow Bow", damage: 8),
+        new Weapon(name: "Stone Hammer", damage: 9),
+        new Weapon(name: "Flame Breath", damage: 12),
+        new Weapon(name: "Soul Drain", damage: 15)
+    };
+
+    foreach (var weapon in weapons)
+    {
+        RegisterWeapon(weapon);
+    }
+}
+
+void RegisterArmors()
+{
+    var armors = new IArmor[]
+    {
+        new Armor(name: "Cloth Shirt", defense: 1),
+        new Armor(name: "Padded Vest", defense: 2),
+        new Armor(name: "Chain Shirt", defense: 4),
+        new Armor(name: "Enchanted Robes", defense: 5),
+        new Armor(name: "Plate Mail", defense: 6),
+        new Armor(name: "Thick Glasses", defense: 1),
+        new Armor(name: "Leather Scraps", defense: 2),
+        new Armor(name: "Matted Fur", defense: 1),
+        new Armor(name: "Patchwork Vest", defense: 2),
+        new Armor(name: "Chain Vest", defense: 3),
+        new Armor(name: "Bone Plating", defense: 2),
+        new Armor(name: "Hooded Cloak", defense: 3),
+        new Armor(name: "Thick Hide", defense: 4),
+        new Armor(name: "Scale Hide", defense: 4),
+        new Armor(name: "Shadow Shroud", defense: 5)
+    };
+
+    foreach (var armor in armors)
+    {
+        RegisterArmor(armor);
+    }
+}
+
+RegisterWeapons();
+RegisterArmors();
 
 Player CreateNewPlayer()
 {
@@ -33,26 +101,26 @@ Player CreateNewPlayer()
         name: "Jackson",
         level: 1,
         health: 100,
-        weapon: GetOrCreateWeapon(name: "Fists", damage: 1),
-        armor: GetOrCreateArmor(name: "Cloth Shirt", defense: 1),
+        weapon: GetWeapon("Fists"),
+        armor: GetArmor("Cloth Shirt"),
         gold: 1000u);
 }
 
 var weaponOffers = new List<(IWeapon weapon, uint cost)>
 {
-    (GetOrCreateWeapon(name: "Dagger", damage: 5), 30u),
-    (GetOrCreateWeapon(name: "Short Sword", damage: 7), 55u),
-    (GetOrCreateWeapon(name: "War Axe", damage: 9), 80u),
-    (GetOrCreateWeapon(name: "Morningstar Flail", damage: 12, factory: () => new SelfDamagingWeapon(name: "Morningstar Flail", damage: 12, selfDamage: 1)), 110u),
-    (GetOrCreateWeapon(name: "Wizard Staff", damage: 11), 100u)
+    (GetWeapon("Dagger"), 30u),
+    (GetWeapon("Short Sword"), 55u),
+    (GetWeapon("War Axe"), 80u),
+    (GetWeapon("Morningstar Flail"), 110u),
+    (GetWeapon("Wizard Staff"), 100u)
 };
 
 var armorOffers = new List<(IArmor armor, uint cost)>
 {
-    (GetOrCreateArmor(name: "Padded Vest", defense: 2), 35u),
-    (GetOrCreateArmor(name: "Chain Shirt", defense: 4), 60u),
-    (GetOrCreateArmor(name: "Enchanted Robes", defense: 5), 75u),
-    (GetOrCreateArmor(name: "Plate Mail", defense: 6), 90u)
+    (GetArmor("Padded Vest"), 35u),
+    (GetArmor("Chain Shirt"), 60u),
+    (GetArmor("Enchanted Robes"), 75u),
+    (GetArmor("Plate Mail"), 90u)
 };
 
 var blacksmith = new Blacksmith(weaponOffers);
@@ -62,24 +130,24 @@ var saveGameService = new SaveGameService(weaponCatalog, armorCatalog);
 
 var levelOneEnemies = new List<Fighter>
 {
-    new Fighter(name: "Nerd", health: 120, weapon: GetOrCreateWeapon(name: "Stick", damage: 1), armor: GetOrCreateArmor(name: "Thick Glasses", defense: 1), gold: 5u),
-    new Fighter(name: "Goblin", health: 80, weapon: GetOrCreateWeapon(name: "Rusty Blade", damage: 4), armor: GetOrCreateArmor(name: "Leather Scraps", defense: 2), gold: 12u),
-    new Fighter(name: "Rabid Dog", health: 70, weapon: GetOrCreateWeapon(name: "Fangs", damage: 5), armor: GetOrCreateArmor(name: "Matted Fur", defense: 1), gold: 8u),
-    new Fighter(name: "Bandit", health: 90, weapon: GetOrCreateWeapon(name: "Shiv", damage: 6), armor: GetOrCreateArmor(name: "Patchwork Vest", defense: 2), gold: 15u)
+    new Fighter(name: "Nerd", health: 120, weapon: GetWeapon("Stick"), armor: GetArmor("Thick Glasses"), gold: 5u),
+    new Fighter(name: "Goblin", health: 80, weapon: GetWeapon("Rusty Blade"), armor: GetArmor("Leather Scraps"), gold: 12u),
+    new Fighter(name: "Rabid Dog", health: 70, weapon: GetWeapon("Fangs"), armor: GetArmor("Matted Fur"), gold: 8u),
+    new Fighter(name: "Bandit", health: 90, weapon: GetWeapon("Shiv"), armor: GetArmor("Patchwork Vest"), gold: 15u)
 };
 
 var levelTwoEnemies = new List<Fighter>
 {
-    new Fighter(name: "Orc Warrior", health: 140, weapon: GetOrCreateWeapon(name: "Heavy Club", damage: 7), armor: GetOrCreateArmor(name: "Chain Vest", defense: 3), gold: 20u),
-    new Fighter(name: "Skeleton Knight", health: 110, weapon: GetOrCreateWeapon(name: "Ancient Sword", damage: 6), armor: GetOrCreateArmor(name: "Bone Plating", defense: 2), gold: 15u),
-    new Fighter(name: "Dark Archer", health: 100, weapon: GetOrCreateWeapon(name: "Shadow Bow", damage: 8), armor: GetOrCreateArmor(name: "Hooded Cloak", defense: 3), gold: 18u),
-    new Fighter(name: "Troll Bruiser", health: 160, weapon: GetOrCreateWeapon(name: "Stone Hammer", damage: 9), armor: GetOrCreateArmor(name: "Thick Hide", defense: 4), gold: 25u)
+    new Fighter(name: "Orc Warrior", health: 140, weapon: GetWeapon("Heavy Club"), armor: GetArmor("Chain Vest"), gold: 20u),
+    new Fighter(name: "Skeleton Knight", health: 110, weapon: GetWeapon("Ancient Sword"), armor: GetArmor("Bone Plating"), gold: 15u),
+    new Fighter(name: "Dark Archer", health: 100, weapon: GetWeapon("Shadow Bow"), armor: GetArmor("Hooded Cloak"), gold: 18u),
+    new Fighter(name: "Troll Bruiser", health: 160, weapon: GetWeapon("Stone Hammer"), armor: GetArmor("Thick Hide"), gold: 25u)
 };
 
 var levelContents = new Dictionary<int, LevelContent>
 {
-    [1] = new LevelContent(levelOneEnemies, new Boss(name: "Dragon Whelp", level: 1, health: 200, weapon: GetOrCreateWeapon(name: "Flame Breath", damage: 12), armor: GetOrCreateArmor(name: "Scale Hide", defense: 4), gold: 50u)),
-    [2] = new LevelContent(levelTwoEnemies, new Boss(name: "Lich Lord", level: 2, health: 260, weapon: GetOrCreateWeapon(name: "Soul Drain", damage: 15), armor: GetOrCreateArmor(name: "Shadow Shroud", defense: 5), gold: 75u))
+    [1] = new LevelContent(levelOneEnemies, new Boss(name: "Dragon Whelp", level: 1, health: 200, weapon: GetWeapon("Flame Breath"), armor: GetArmor("Scale Hide"), gold: 50u)),
+    [2] = new LevelContent(levelTwoEnemies, new Boss(name: "Lich Lord", level: 2, health: 260, weapon: GetWeapon("Soul Drain"), armor: GetArmor("Shadow Shroud"), gold: 75u))
 };
 
 var player = InitializePlayer(saveGameService);
