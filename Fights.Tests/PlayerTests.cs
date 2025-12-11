62587f7e7b6424a6d83915796b55df6b2c30ccef
Jackson Buck <mout12@gmail.com>
2025-12-10 22:47:07 -0800

    Add another unit test

--- Fights.Tests/PlayerTests.cs
+++ Fights.Tests/PlayerTests.cs
@@ -16,4 +16,16 @@ public sealed class PlayerTests
 
         Assert.AreEqual(2, player.Level);
     }
+
+    [TestMethod]
+    public void TakeDamage_ReducesHealthByDamageMinusArmor()
+    {
+        var weapon = new Weapon("Starter Sword", 10);
+        var armor = new Armor("Leather Armor", 5);
+        var player = new Player("Rookie", 1, 100, weapon, armor, 25);
+
+        var dmg = new DamagePayload(10, 0, false);
+        player.TakeDamage(dmg);
+        Assert.AreEqual(90, player.Health);
+    }
 }
